1c04a603c1d2f8fa5874317df714c4cc96d1a288
Labidahrom <labidahrom@yandex.ru>
2022-07-28 11:07:59 +0300

    add buttons to bot

--- hh_bot.py
+++ hh_bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 from hh_parser import HHParser
 
-API_TOKEN = '!!!!!!!!!!!!!!!!токена нет!!!!!!!!!!!!!!!!!!!!!!!!'
+API_TOKEN = '5441322876:AAE_tmZYENJrNXBurTRlKZH6UsQN71L5LXk'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -29,11 +29,13 @@ def vacancy_name_step(message):
 
 
 def start_hh_parser(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Количество вакансий', 'Средняя зарплата', 'Пример вакансии')
     HhRequest.vacancy_city = message.text
     parser_info = HHParser.hhparser(HhRequest.vacancy_name, HhRequest.vacancy_city)
     if len(parser_info) == 3:
-        bot.send_message(message.from_user.id,
-                         str(parser_info[0]) + ' ' + str(parser_info[1]) + ' ' + str(parser_info[2]))
+        bot.send_message(message.from_user.id, 'Выберите что вас интересует:', reply_markup=markup,
+                         allow_sending_without_reply=True)
     else:
         bot.send_message(message.from_user.id, parser_info)
     bot.register_next_step_handler(message, send_welcome)
