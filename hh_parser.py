cc322209bcdbd8c298b6a928fe8831bd9b7e4ce3
Labidahrom <labidahrom@yandex.ru>
2022-07-30 18:26:30 +0300

    add wrong city name exeption

--- hh_parser.py
+++ hh_parser.py
@@ -1,17 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import re
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from bs4 import BeautifulSoup
 import requests
 
 
 class HHParser:
-    def test_module(self):
-        print('eeee')
-
     first_vacancy_link =''
 
     def hhparser(vacancy_name, city):
@@ -25,38 +20,42 @@ class HHParser:
         change_city_link.click()
         find_city_field = search_for_element((By.CLASS_NAME, 'HH-AreaSwitcherSearch-Input'))
         find_city_field.send_keys(city)
-        choose_city = search_for_element((By.CSS_SELECTOR, '.area-switcher-autocomplete-item .bloko-text.bloko-text_strong'))
-        choose_city.click()
+        time.sleep(1)
+        city_search_suggestion = browser.find_elements(By.CSS_SELECTOR, '.area-switcher-autocomplete-item')
+        if len(city_search_suggestion) < 1:
+            return "Не найден город"
+        time.sleep(1)
+        choose_city_link = search_for_element((By.CSS_SELECTOR, '.area-switcher-autocomplete-item .bloko-text.bloko-text_strong'))
+        choose_city_link.click()
         time.sleep(2)
-        vacancy_search = search_for_element((By.ID, 'a11y-search-input'))
-        vacancy_search.send_keys(vacancy_name)
+        search_vacancy_field = search_for_element((By.ID, 'a11y-search-input'))
+        search_vacancy_field.send_keys(vacancy_name)
         time.sleep(1)
-        button_search = search_for_element((By.CSS_SELECTOR, '[data-page-analytics-event="searchButton.submit"]'))
-        button_search.click()
-        salar = browser.find_elements(By.CSS_SELECTOR, '.bloko-radio__text [data-qa="serp__novafilter-title"]')
+        search_vacancy_button = search_for_element((By.CSS_SELECTOR, '[data-page-analytics-event="searchButton.submit"]'))
+        search_vacancy_button.click()
+        salary_block = browser.find_elements(By.CSS_SELECTOR, '.bloko-radio__text [data-qa="serp__novafilter-title"]')
         time.sleep(1)
-        vacancy_number = browser.find_elements(By.CSS_SELECTOR, '[data-qa="vacancies-total-found"] span')
-        if len(vacancy_number) < 1:
+        vacancy_amount_element = browser.find_elements(By.CSS_SELECTOR, '[data-qa="vacancies-total-found"] span')
+        if len(vacancy_amount_element) < 1:
             return "Не найдено результатов"
-        vacancy_amount = vacancy_number[0].text.replace(', ', '')
+        vacancy_amount = vacancy_amount_element[0].text.replace(', ', '')
         time.sleep(1)
-        solar_list = [''.join([f for f in i.text if f.isdigit()]) for i in salar if 'от' in i.text]
+        salary_list = [''.join([f for f in i.text if f.isdigit()]) for i in salary_block if 'от' in i.text]
         time.sleep(1)
         sr_count = browser.find_elements(By.CSS_SELECTOR,
                                          '.bloko-radio [name="salary"] + span .bloko-text.bloko-text_tertiary')
         total_amount = 0
         quantity = 0
         for i in range(len(sr_count)):
-            total_amount += int(solar_list[i]) * int(sr_count[i].text)
+            total_amount += int(salary_list[i]) * int(sr_count[i].text)
             quantity += int(sr_count[i].text)
-        if solar_list:
+        if salary_list:
             average_salary = int(total_amount / quantity)
         else:
             average_salary = 'Нет данных'
-        first_vacancy = browser.find_elements(By.CSS_SELECTOR, '[data-qa="vacancy-serp__vacancy-title"]')[0]
+        first_vacancy_element = browser.find_elements(By.CSS_SELECTOR, '[data-qa="vacancy-serp__vacancy-title"]')[0]
         global first_vacancy_link
-        first_vacancy_link = first_vacancy.get_attribute('href')
-        # print(first_vacancy_link)
+        first_vacancy_link = first_vacancy_element.get_attribute('href')
         return vacancy_amount, average_salary, first_vacancy_link
         browser.quit()
 
