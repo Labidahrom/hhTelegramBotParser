354b27c3e1b7181cc74f79555db62cb486157405
Labidahrom <labidahrom@yandex.ru>
2022-08-03 07:08:49 +0300

    parser is ready

--- hh_bot.py
+++ hh_bot.py
@@ -39,24 +39,57 @@ def start_hh_parser(message):
     if len(parser_info) == 3:
         bot.send_message(message.from_user.id, 'Выберите что вас интересует:', reply_markup=markup,
                          allow_sending_without_reply=True)
-        HHParser.vacancy_amount = parser_info[0]
-        HHParser.vacancy_salary = parser_info[1]
-        HHParser.vacancy_link = parser_info[2]
+        HhRequest.vacancy_amount = parser_info[0]
+        HhRequest.vacancy_salary = parser_info[1]
+        HhRequest.vacancy_link = parser_info[2]
     else:
         bot.send_message(message.from_user.id, parser_info)
     bot.register_next_step_handler(message, choose_option)
 
+
 def choose_option(message):
     if message.text == "Количество вакансий":
-        bot.send_message(message.from_user.id, HHParser.vacancy_amount)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Вернуться назад')
+        bot.send_message(message.from_user.id, HhRequest.vacancy_amount, reply_markup=markup,
+                         allow_sending_without_reply=True)
     elif message.text == "Средняя зарплата":
-        bot.send_message(message.from_user.id, HHParser.vacancy_salary)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Вернуться назад')
+        bot.send_message(message.from_user.id, HhRequest.vacancy_salary, reply_markup=markup,
+                         allow_sending_without_reply=True)
     elif message.text == "Пример вакансии":
-        vacancy_description = HHParser.get_first_vacancy(HHParser.vacancy_link)
-        bot.send_message(message.from_user.id, vacancy_description)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Вернуться назад')
+        HhRequest.vacancy_description = HHParser.get_first_vacancy(HhRequest.vacancy_link)
+        bot.send_message(message.from_user.id, HhRequest.vacancy_description, reply_markup=markup,
+                         allow_sending_without_reply=True)
     else:
         bot.send_message(message.from_user.id, 'Ниче не выбрал')
-    bot.register_next_step_handler(message, send_welcome)
+    bot.register_next_step_handler(message, second_step)
+
+
+def second_step(message):
+    if message.text == "Вернуться назад":
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Количество вакансий', 'Средняя зарплата', 'Пример вакансии')
+        bot.send_message(message.from_user.id, HhRequest.vacancy_amount, reply_markup=markup,
+                         allow_sending_without_reply=True)
+        bot.register_next_step_handler(message, choose_option)
+    elif message.text == "Средняя зарплата":
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Количество вакансий', 'Пример вакансии')
+        bot.send_message(message.from_user.id, HhRequest.vacancy_salary, reply_markup=markup,
+                         allow_sending_without_reply=True)
+        bot.register_next_step_handler(message, choose_option)
+    elif message.text == "Пример вакансии":
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Количество вакансий', 'Средняя зарплата')
+        bot.send_message(message.from_user.id, vacancy_description, reply_markup=markup,
+                         allow_sending_without_reply=True)
+    else:
+        bot.send_message(message.from_user.id, 'Ниче не выбрал')
+        bot.register_next_step_handler(message, send_welcome)
 
 
 bot.enable_save_next_step_handlers(delay=2)
